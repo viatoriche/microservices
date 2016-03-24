56df857dff75017000f550db15f4d64a89191e61
viatoriche (Максим Панфилов) <mpanfilov@npoprogress.com>
2016-03-24 15:11:21 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
+import os
 from setuptools import find_packages, setup
 
-version = __import__('microservices').get_version()
+package = 'microservices'
+
+version = __import__(package).get_version()
 packages = find_packages()
 
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
 setup(
-    name='microservices',
+    name=package,
     version=version,
     url='',
     packages=packages,
-    package_data={'microservices': ["templates/*.*"]},
+    package_data=get_package_data(package),
     license='',
     author='viatoriche',
     author_email='mpanfilov@npoprogress.com',
