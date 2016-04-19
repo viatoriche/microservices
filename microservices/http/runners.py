416f8e6398f9566b49bd89fa271698404fbaff4b
viatoriche (Максим Панфилов) <mpanfilov@npoprogress.com>
2016-04-19 09:44:05 +0300

    fix runners

--- microservices/http/runners.py
+++ microservices/http/runners.py
@@ -1,17 +1,17 @@
-def gevent_run(app, port, log, error_log):
+def gevent_run(app, port, log, error_log, **kwargs):
     from gevent.wsgi import WSGIServer
     from gevent import monkey
 
     monkey.patch_all()
-    http_server = WSGIServer(('', port), app, log=log, error_log=error_log)
+    http_server = WSGIServer(('', port), app, log=log, error_log=error_log, **kwargs)
     http_server.serve_forever()
 
 
-def base_run(app, port=8080):
+def base_run(app, port=8080, **kwargs):
     import logging
     import os
     logging.basicConfig(
         level=getattr(logging, os.environ.get('LOG_LEVEL', 'DEBUG')),
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
-    app.run(port=port)
+    app.run(port=port, **kwargs)
