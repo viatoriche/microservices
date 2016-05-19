c87beb31cbe7fd362541df3a0019f466fd2e6e9e
viatoriche (Максим Панфилов) <mpanfilov@npoprogress.com>
2016-05-19 13:25:01 +0300

    fix: importing in runners

--- microservices/http/runners.py
+++ microservices/http/runners.py
@@ -1,8 +1,4 @@
-from microservices.utils import get_logger, set_logging
-
-
 def base_run(app, port=8080, **kwargs):
-    set_logging()
     app.run(port=port, **kwargs)
 
 
@@ -10,11 +6,10 @@ def gevent_run(app, port, log, error_log, **kwargs):
     try:
         from gevent.wsgi import WSGIServer
         from gevent import monkey
-    except ImportError:
-        set_logging()
-        get_logger().warning('gevent not installed, running in base mode')
-        base_run(app, port, **kwargs)
 
-    monkey.patch_all()
-    http_server = WSGIServer(('', port), app, log=log, error_log=error_log, **kwargs)
-    http_server.serve_forever()
+        monkey.patch_all()
+        http_server = WSGIServer(('', port), app, log=log, error_log=error_log, **kwargs)
+        http_server.serve_forever()
+    except ImportError:
+        error_log.warning('gevent not installed, running in base mode')
+        base_run(app, port, **kwargs)
