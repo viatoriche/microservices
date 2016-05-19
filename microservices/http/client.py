54f8caa0234fff2c271e5b13c7a812c7f0427a7a
viatoriche (Максим Панфилов) <mpanfilov@npoprogress.com>
2016-05-19 13:17:50 +0300

    add: tests for client, logger, resource builder for resource, endpoint with path

--- microservices/http/client.py
+++ microservices/http/client.py
@@ -3,6 +3,8 @@ import urlparse
 
 import requests
 
+from microservices.utils import get_logger
+
 
 class ResponseError(Exception):
     def __init__(self, response, description, *args, **kwargs):
@@ -27,10 +29,11 @@ class _client_request(object):
         self.client = client
         self.resources = resources
         self.method = method
+        self.logger = client.logger
 
     def __call__(self, *args, **kwargs):
         method = getattr(self.client, self.method)
-        args = self.resources + args
+        args = tuple(self.resources) + args
         return method(*args, **kwargs)
 
 
@@ -39,12 +42,11 @@ class _requests_method(object):
         self.method = method
         self.client = client
         self.close_slash = client.close_slash
+        self.logger = client.logger
 
     def build_resource(self, resources):
         resource = '/'.join(resources)
-        if self.close_slash:
-            if not resource.endswith('/'):
-                resource += '/'
+        self.logger.debug('Resource {} builded from {}'.format(resource, resources))
         return resource
 
     def __call__(self, *resources, **kwargs):
@@ -58,7 +60,9 @@ class _requests_method(object):
         resource = self.build_resource(resources)
         if data is not None:
             kwargs['json'] = data
-        response = requests.request(self.method, self.client.url_for(resource, query), timeout=timeout, **kwargs)
+        url = self.client.url_for(resource, query)
+        self.logger.info('{}: {}'.format(self.method, url))
+        response = requests.request(self.method, url, timeout=timeout, **kwargs)
         return self.client.handle_response(response, response_key=response_key)
 
 
@@ -66,16 +70,26 @@ class Resource(object):
     def __init__(self, client, resources):
         self.client = client
         self.resources = resources
+        self.logger = client.logger
 
     def __getattr__(self, item):
         return _client_request(self.client, self.resources, item)
+
+    def resource(self, *resources):
+        resources = tuple(self.resources) + resources
+        return Resource(self.client, resources)
 
 
 class Client(object):
     ok_statuses = (200, 202,)
     to_none_statuses = (404,)
 
-    def __init__(self, endpoint, ok_statuses=None, to_none_statuses=None, empty_to_none=True, close_slash=True):
+    def __init__(self, endpoint, ok_statuses=None, to_none_statuses=None, empty_to_none=True, close_slash=True,
+                 logger=None):
+        if logger is None:
+            logger = get_logger(__name__)
+
+        self.logger = logger
         if endpoint.endswith('/'):
             endpoint = endpoint[:-1]
         if ok_statuses is not None:
@@ -84,14 +98,29 @@ class Client(object):
             self.to_none_statuses = to_none_statuses
         self.empty_to_none = empty_to_none
         self.close_slash = close_slash
+        parsed_url = urlparse.urlparse(endpoint)
+        endpoint = self.get_endpoint_from_parsed_url(parsed_url)
         self.endpoint = endpoint
+        self.path = parsed_url.path
+        self.logger.debug('Client builded for endpoint {} and path {}'.format(self.endpoint, self.path))
 
-    def url_for(self, resource='/', query=None):
+    @staticmethod
+    def get_endpoint_from_parsed_url(parsed_url):
+        url_list = [(lambda: x if e < 2 else '')() for e, x in enumerate(list(parsed_url))]
+        return urlparse.urlunparse(url_list)
+
+    def url_for(self, resource='', query=None):
         parsed_url = list(urlparse.urlparse(self.endpoint))
-        parsed_url[2] = resource
+        path = self.path + '/' + resource
+        if self.close_slash:
+            if not path.endswith('/'):
+                path += '/'
+        parsed_url[2] = path
         if query is not None:
             parsed_url[4] = urllib.urlencode(query, doseq=1)
-        return urlparse.urlunparse(parsed_url)
+        url = urlparse.urlunparse(parsed_url)
+        self.logger.debug('Url {} builded for resource {}'.format(url, resource))
+        return url
 
     def handle_response(self, response, response_key=None):
         status_code = response.status_code
@@ -108,6 +137,8 @@ class Client(object):
                     raise ResponseError(response, 'Response key not found!')
             elif response_key is not None and status_code in self.to_none_statuses:
                 result = None
+            elif status_code not in self.ok_statuses and status_code not in self.to_none_statuses:
+                raise ResponseError(response, 'Status code {} not in ok_statuses {}'.format(status_code, self.ok_statuses))
         if response_key is not None and self.empty_to_none and result is not None and not result:
             result = None
 
--- microservices/http/test.py
+++ microservices/http/test.py
@@ -8,6 +8,26 @@ class TestHTTP(unittest.TestCase):
     def tearDown(self):
         pass
 
+import requests
+
+class MockRequest(object):
+
+    def __init__(self, handler=lambda instance, *args, **kwargs: None, **kwargs):
+        self.response = requests.Response()
+        self.handler = handler
+
+        for k, v in kwargs.iteritems():
+            setattr(self.response, k, v)
+
+    def handle(self, *args, **kwargs):
+        self.handler(self, *args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        self.handle(*args, **kwargs)
+        return self.response
+
+def patch_requests(request):
+    requests.request = request
 
 class TestService(TestHTTP):
 
@@ -44,3 +64,77 @@ class TestService(TestHTTP):
         self.assertEqual(data['status'], '200 OK')
         self.assertEqual(data['info']['resource'], 'INFO')
         self.assertEqual(data['status_code'], 200)
+
+class TestClient(unittest.TestCase):
+
+    def test_client(self):
+        from microservices.http.client import Client, ResponseError
+        from microservices.utils import set_logging
+
+        set_logging()
+
+        valid_method = 'get'
+        valid_url = 'http://user:password@endpoint:8000/test/1/'
+        valid_json = None
+
+        def test_request(instance, method, url, **kwargs):
+            self.assertEqual(method, valid_method)
+            self.assertEqual(url, valid_url)
+            self.assertEqual(kwargs.get('json', None), valid_json)
+
+        patch_requests(MockRequest(handler=test_request, _content='{"response": "tested"}', status_code=200))
+
+        client = Client('http://user:password@endpoint:8000/test/1')
+        response = client.get()
+
+        self.assertEqual(response, {'response': 'tested'})
+        response = client.get(response_key='response')
+        self.assertEqual(response, 'tested')
+        self.assertEqual(client.endpoint, 'http://user:password@endpoint:8000')
+        self.assertEqual(client.path, '/test/1')
+
+        client = Client('http://user:password@endpoint:8000/test/1/')
+
+        valid_url = 'http://user:password@endpoint:8000/test/1/jopa/'
+        client.get('jopa', response_key='response')
+
+        valid_json = {'test': 'tested'}
+        valid_method = 'post'
+        valid_url = 'http://user:password@endpoint:8000/test/1/jopa/?test=tested'
+        client.post('jopa', query={'test': 'tested'}, response_key='response', data=valid_json)
+        client.post('jopa', query={'test': 'tested'}, key='response', data=valid_json)
+
+        jopa = client.resource('jopa')
+        jopa.post(query={'test': 'tested'}, key='response', data=valid_json)
+
+        valid_url = 'http://user:password@endpoint:8000/test/1/jopa/1/2/3/?test=tested'
+        jopa.post('1', '2', '3', query={'test': 'tested'}, key='response', data=valid_json)
+        jopa.post('1/2', '3', query={'test': 'tested'}, key='response', data=valid_json)
+        jopa.post('1/2', '3/', query={'test': 'tested'}, key='response', data=valid_json)
+        jopa1 = jopa.resource('1')
+        jopa1.post('2', '3', query={'test': 'tested'}, key='response', data=valid_json)
+
+        client = Client('http://endpoint/', ok_statuses=(200, 202, ), to_none_statuses=(404, ))
+        valid_url = 'http://endpoint/'
+        valid_json = None
+        valid_method = 'get'
+        patch_requests(MockRequest(handler=test_request, _content='{"response": "tested"}', status_code=404))
+        client.get(key='response')
+        client.get(key='response_no')
+        patch_requests(MockRequest(handler=test_request, _content='{"response": "tested"}', status_code=200))
+        client.get(key='response')
+        self.assertRaises(ResponseError, client.get, key='response_no')
+        patch_requests(MockRequest(handler=test_request, _content='{"response": "tested"}', status_code=500))
+        self.assertRaises(ResponseError, client.get, key='response')
+
+        patch_requests(MockRequest(handler=test_request, _content='{"response": ""}', status_code=200))
+        response = client.get(key='response')
+        self.assertEqual(response, None)
+
+
+        client = Client('http://endpoint/', empty_to_none=False)
+        response = client.get(key='response')
+        self.assertEqual(response, "")
+
+        patch_requests(MockRequest(handler=test_request, _content='bad', status_code=200))
+        self.assertRaises(ResponseError, client.get, key='response')
