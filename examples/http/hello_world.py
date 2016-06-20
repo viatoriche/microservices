6a051c3fc022e74a88e7f6ad95218c3b4020f7da
viatoriche / Maxim Panfilov <Maxim.Panfilov@acronis.com>
2016-06-20 15:30:18 +0300

    settings

--- examples/http/hello_world.py
+++ examples/http/hello_world.py
@@ -4,24 +4,27 @@ import datetime
 
 app = Microservice(__name__)
 
+app.config['SCHEMA'] = ResourceSchema(
+    response='result',
+    response_update=False,
+    status_code='status',
+    browser=BrowserResourceSchema(
+        status=None,
+    )
+)
+
+
 @app.route(
     '/',
     resource=ResourceMarker(
         update={
             'resource_created': datetime.datetime.now().isoformat()
         },
-        schema=ResourceSchema(
-            response='result',
-            response_update=False,
-            status_code='status',
-            browser=BrowserResourceSchema(
-                status=None,
-            )
-        )
     ),
 )
 def hello_world():
     return {'hello': 'Hello world'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
