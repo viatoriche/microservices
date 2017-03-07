5c33f5fb8c82a35a05e115e643e13d9fb94aed4d
viatoriche / Maxim Panfilov <Maxim.Panfilov@acronis.com>
2017-03-07 19:05:25 +0300

    fix: add_resource in http

--- microservices/http/service.py
+++ microservices/http/service.py
@@ -5,6 +5,7 @@ from flask_api import FlaskAPI
 from microservices.utils import dict_update
 
 from microservices.http.settings import MicroserviceAPISettings
+from microservices.http.resources import ResourceSchema
 from functools import reduce
 
 from flask_api import app
@@ -41,12 +42,15 @@ class Microservice(FlaskAPI):
             resource['endpoints'] = endpoints
             resource['methods'] = methods
 
-            schema = self.api_settings.SCHEMA
-            schema.update(resource.get('schema', {}))
+            schema = ResourceSchema()
+            schema.update(self.api_settings.SCHEMA)
+            resource_schema = resource.get('schema', {})
+            if resource_schema:
+                schema.update(resource_schema)
             resource['schema'] = schema
             in_resources = resource.get('in_resources')
             if in_resources is None:
-                in_resources = self.api_settings.IN_RESOURCES
+                in_resources = self.api_settings.IN_RESOURCES[:]
             resource['in_resources'] = in_resources
             self.resources[rule] = dict_update(orig_resource, resource)
 
